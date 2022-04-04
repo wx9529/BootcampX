ea510b907b4f4e49706da75844ee69d9e01d9f09
wangxin <xinwang213z@gmail.com>
2022-04-04 23:31:43 +0000

    BootcampX Queries 3 - Group By

--- 2_queries_joins/2_CohortTotalAssignmentDuration.sql
+++ 2_queries_joins/2_CohortTotalAssignmentDuration.sql
@@ -3,3 +3,11 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
+
+
+SELECT count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = student_id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING count(assignment_submissions.*) <100;
